c2a8fbb6a3ba6e1aed2aa722c3f7bc0859a049ab
TlumaczeniaKlikacze <89144762+TlumaczeniaKlikacze@users.noreply.github.com>
2022-06-22 14:01:38 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+const FONT_SIZE = 5
+const WIDTH = 300
+const HEIGHT = 400
+
 const express = require('express')
 const app = express()
 const csv = require('csvtojson')
-
+const PDFDocument = require('pdfkit')
+const getStream = require('get-stream')
 const id_display = `IAI(id) towaru`
 const ean_display = `Kod producenta (ten z produktu prawdziwy)`
-
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(express.json())
 let list_of_object = []
@@ -26,6 +31,41 @@ app.post('/getByEan',(req,res)=>{
     }
     return res.status(400).json({message:'Opis dla takiego EANU/ID nie istnieje'})
 })
+app.post('/getBase64',async(req,res)=>{
+    try {
+        const text = req.body.text
+        const pdf = async() => {
+            const doc = new PDFDocument({
+                size:[WIDTH,HEIGHT],
+                margins:{
+                    top:20,
+                    bottom:0,
+                    left:20,
+                    right:0
+                }
+            });
+            doc
+            .fontSize(FONT_SIZE)
+            .font('./fonts/NotoSans-Light.ttf')
+            .text(text)
+            doc.end()
+            return await getStream.buffer(doc)
+          }
+          
+          
+          // Caller could do this:
+          const pdfBuffer = await pdf()
+          const pdfBase64string = pdfBuffer.toString('base64')
+
+          return res.json({base:pdfBase64string})
+
+    } catch (error) {
+        console.log(error)
+        return res.json({base:null})
+    }
+    
+})
+
 app.get('/',(req,res)=>{
     res.render('index.ejs')
 })
