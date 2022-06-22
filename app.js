0356be760779e96bee495c16cb4fc8f153bc2f64
TlumaczeniaKlikacze <89144762+TlumaczeniaKlikacze@users.noreply.github.com>
2022-06-22 09:32:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,18 +8,7 @@ const ean_display = `Kod producenta (ten z produktu prawdziwy)`
 app.set('view engine', 'ejs')
 app.use(express.json())
 let list_of_object = []
-app.post('/getById',(req,res)=>{
-    const id = req.body.id
-    const find = list_of_object.find(e=>e[id_display] == id)
-    if(find){
-        const newOne = {...find}
 
-        delete newOne[id_display]
-        delete newOne[ean_display]
-        return res.status(200).json({data:newOne})
-    }
-    return res.status(400).json({message:'Opis dla takiego id nie istnieje'})
-})
 app.post('/getByEan',(req,res)=>{
     const id = req.body.id
     const find = list_of_object.find(e=>e[ean_display] == id)
@@ -35,7 +24,7 @@ app.post('/getByEan',(req,res)=>{
         delete newOne2[ean_display]
         return res.status(200).json({data:newOne2})
     }
-    return res.status(400).json({message:'Opis dla takiego EANU nie istnieje'})
+    return res.status(400).json({message:'Opis dla takiego EANU/ID nie istnieje'})
 })
 app.get('/',(req,res)=>{
     res.render('index.ejs')
